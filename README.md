3239da7a53ab6de2c3ae425e53f7a8a67c4d0dfc
demilsonpereira <demilson.d2102@gmail.com>
2021-08-13 16:27:50 -0400

    Done

---
+++ README.md
@@ -0,0 +1,48 @@
+# Componentizando a aplicação
+
+Este projeto foi desenvolvido no desafio #02 do curso de Reactjs da Rocketseat. O projeto consiste em um guia de filmes categorizados por uma lista de generos. Como adicional, eu acrescentei um componente que exibe todas as informações do filme ao serem clicados.
+
+![Componente principal](src/assets/example1.png)
+![Componente extra](src/assets/example2.png)
+
+# Installation
+
+You'll need YARN to install this application.
+
+Open your bash and run:
+
+```bash
+git clone https://github.com/DemilsonPereira/desafio02-componentizando-aplicacao.git componentizando-a-aplicacao
+```
+
+```bash
+cd desafio02-componentizando-aplicacao
+```
+
+```bash
+yarn install
+```
+
+## Usage
+
+First you need to load the api
+
+```bash
+yarn server
+```
+
+Then start the application
+
+```bash
+yarn dev
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,64 @@
-export function Content() {
-  // Complete aqui
+import { memo } from "react";
+import { MovieCard } from "./MovieCard";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
 }
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+type ContentProps = {
+  selectedGenre: GenreResponseProps;
+  movies: MovieProps[];
+  handleSelectMovie(imdbId: string): void;
+};
+
+export const Content = memo(
+  ({ selectedGenre, movies, handleSelectMovie }: ContentProps) => {
+    return (
+      <>
+        <header>
+          <span className="category">
+            Categoria:<span> {selectedGenre.title}</span>
+          </span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map((movie) => (
+              <a
+                key={movie.Title}
+                onClick={() => handleSelectMovie(movie.imdbID)}
+              >
+                <MovieCard
+                  key={movie.imdbID}
+                  title={movie.Title}
+                  poster={movie.Poster}
+                  runtime={movie.Runtime}
+                  rating={movie.Ratings[0].Value}
+                />
+              </a>
+            ))}
+          </div>
+        </main>
+      </>
+    );
+  },
+  (prevProps, nextProps) => {
+    return (
+      Object.is(prevProps.selectedGenre, nextProps.selectedGenre) ||
+      Object.is(prevProps.movies, nextProps.movies)
+    );
+  }
+);
